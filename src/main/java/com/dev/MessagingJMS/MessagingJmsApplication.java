ed428df163d00ccd24435332e57f95b1a955d7c5
Kevin Neag <neagkv@gmail.com>
2018-08-28 21:44:18 -0400

    new repo

---
+++ src/main/java/com/dev/MessagingJMS/MessagingJmsApplication.java
@@ -0,0 +1,12 @@
+package com.dev.MessagingJMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingJmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagingJmsApplication.class, args);
+	}
+}
